bb62cba7170fbf64dd26272dde44bc3135a2633b
Amirsina Torfi <amirsina.torfi@gmail.com>
2017-04-15 12:27:27 -0400

    modify welcome

--- Tutorials/0-welcome/code/1-welcome.py
+++ Tutorials/0-welcome/code/1-welcome.py
@@ -2,6 +2,9 @@
 ########## Welcome to TensorFlow World ##############
 #####################################################
 
+# The tutorials in this section is just a start for going into TensorFlow world.
+# The TensorFlow flags are used for having a more user friendly environment.
+
 from __future__ import print_function
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
@@ -10,7 +13,9 @@ import os
 
 ######################################
 ######### Necessary Flags ############
-######################################
+# ####################################
+
+# The default path for saving event files is the same folder of this python file.
 tf.app.flags.DEFINE_string(
     'log_dir', os.path.dirname(os.path.abspath(__file__)) + '/logs',
     'Directory where event logs are written to.')
@@ -23,7 +28,9 @@ FLAGS = tf.app.flags.FLAGS
 ################################################
 
 # The user is prompted to input an absolute path.
-if not os.path.isabs(FLAGS.log_dir):
+# os.path.expanduser is leveraged to transform '~' sign to the corresponding path indicator.
+#       Example: '~/logs' equals to '/home/username/logs'
+if not os.path.isabs(os.path.expanduser(FLAGS.log_dir)):
     raise ValueError('You must assign absolute path for --log_dir')
 
 
@@ -37,7 +44,7 @@ y = tf.div(a, b, name="divide")
 
 # Run the session
 with tf.Session() as sess:
-    writer = tf.summary.FileWriter('log_dir', sess.graph)
+    writer = tf.summary.FileWriter(os.path.expanduser(FLAGS.log_dir), sess.graph)
     print("a =", sess.run(a))
     print("b =", sess.run(b))
     print("a + b =", sess.run(x))
