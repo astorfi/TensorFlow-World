c8ab7b2d4975e7d827322529fa73c172f1e30646
iramirsina <amirsina.torfi@gmail.com>
2017-06-14 16:53:50 -0400

    welcome revised

--- codes/0-welcome/code/1-welcome.py
+++ codes/0-welcome/code/1-welcome.py
@@ -14,31 +14,18 @@ import os
 ######### Necessary Flags ############
 # ####################################
 
-# The default path for saving event files is the same folder of this python file.
-tf.app.flags.DEFINE_string(
-    'log_dir', os.path.dirname(os.path.abspath(__file__)) + '/logs',
-    'Directory where event logs are written to.')
-
-# Store all elemnts in FLAG structure!
-FLAGS = tf.app.flags.FLAGS
+log_dir = os.path.dirname(os.path.abspath(__file__)) + '/logs'
 
 ################################################
 ################# handling errors!##############
 ################################################
-
-# The user is prompted to input an absolute path.
-# os.path.expanduser is leveraged to transform '~' sign to the corresponding path indicator.
-#       Example: '~/logs' equals to '/home/username/logs'
-if not os.path.isabs(os.path.expanduser(FLAGS.log_dir)):
-    raise ValueError('You must assign absolute path for --log_dir')
-
 
 # Defining some sentence!
 welcome = tf.constant('Welcome to TensorFlow world!')
 
 # Run the session
 with tf.Session() as sess:
-    writer = tf.summary.FileWriter(os.path.expanduser(FLAGS.log_dir), sess.graph)
+    writer = tf.summary.FileWriter(os.path.expanduser(log_dir), sess.graph)
     print("output: ", sess.run(welcome))
 
 # Closing the writer.
