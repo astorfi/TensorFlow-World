71652ea17dd1b43ad4f3e73ae9ec7e4b84d93a19
iramirsina <amirsina.torfi@gmail.com>
2017-06-29 00:13:28 -0400

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -93,6 +93,14 @@ html_theme = 'alabaster'
 #
 # html_theme_options = {}
 
+html_theme_options = {
+    'show_powered_by': False,
+    'github_user': 'astorfi',
+    'github_repo': 'TensorFlow-World',
+    'github_banner': True,
+    'show_related': False
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
